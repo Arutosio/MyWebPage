10b37c3fd5cfba6b7d5168acd7b9a1e6be2cb970
Arutosio <Arutosio@outlook.com>
2020-12-05 17:53:53 +0100

    Added the new evidences of the sections page

--- js/controller.js
+++ js/controller.js
@@ -1,42 +1,64 @@
 document.addEventListener('DOMContentLoaded', function(event) {
     //the event occurred
     
-    //ElementRef
+    //ElementRef ele = element(tag HTML)
+    // Sec = section
     var eleSelections = document.getElementById("Selections");
     var eleSecHome = document.getElementById("home");
     var eleSecFeaturedon = document.getElementById("featuredon");
     var eleSecAboutMe = document.getElementById("aboutMe");
     var eleSecDev = document.getElementById("dev");
+    // A = a
+    var eleAHome = document.getElementById("aHome");
+    var eleAFeaturedon = document.getElementById("aFeaturedon");
+    var eleAAboutMe = document.getElementById("aAboutMe");
+    var eleADev = document.getElementById("aDev");
 
     //#region EVENT LISTENERS
     //document.body.getElementById("Selections").addEventListener("click", ShowSelection(x));
 
-    document.addEventListener('click', function( event ) {
-        if (eleSelections !== event.target && eleSelections.contains(event.target)) {
+    document.addEventListener('click', function( event ) 
+    {
+        if (eleSelections !== event.target && eleSelections.contains(event.target)) 
+        {
             let nameSelection = event.target.getAttribute("href").substring(1);
             //Show and Hiden SelectionHome
             if(nameSelection == eleSecHome.getAttribute("id")) {
-                console.log(nameSelection);
                 eleSecHome.style.display = "flex";
-            } else { eleSecHome.style.display = "none"; }
+                eleAHome.classList.add("active");
+                console.log(eleSecHome.className);
+            } else { 
+                eleSecHome.style.display = "none";
+                eleAHome.classList.remove("active");
+                console.log(eleSecHome.className);
+            }
 
             //Show and Hiden SelectionFeaturedon
             if(nameSelection == eleSecFeaturedon.getAttribute("id")) {
-                console.log(nameSelection);
                 eleSecFeaturedon.style.display = "flex";
-            } else { eleSecFeaturedon.style.display = "none"; }
+                eleAFeaturedon.classList.add("active");
+            } else { 
+                eleSecFeaturedon.style.display = "none";
+                eleAFeaturedon.classList.remove("active");
+            }
 
             //Show and Hiden SelectionAboutMe
             if(nameSelection == eleSecAboutMe.getAttribute("id")) {
-                console.log(nameSelection);
                 eleSecAboutMe.style.display = "flex";
-            } else { eleSecAboutMe.style.display = "none"; }
+                eleAAboutMe.classList.add("active");
+            } else {
+                eleSecAboutMe.style.display = "none";
+                eleAAboutMe.classList.remove("active"); 
+            }
 
             //Show and Hiden SelectionDev
             if(nameSelection == eleSecDev.getAttribute("id")) {
-                console.log(nameSelection);
                 eleSecDev.style.display = "flex";
-            } else { eleSecDev.style.display = "none"; }
+                eleADev.classList.add("active");
+            } else {
+                eleSecDev.style.display = "none";
+                eleADev.classList.remove("active");
+            }
         }
     });
     //#endregion EVENT LISTENERS
