132875f5320e602887f0051ca4f7a6afea9f0cf8
Arutosio <Arutosio@outlook.com>
2021-12-09 21:43:40 +0100

    CHnage name folder Video and Audio

--- js/main.js
+++ js/main.js
@@ -36,7 +36,7 @@ document.addEventListener('DOMContentLoaded', function(event) {
             if(nameSelection == eleSecHome.getAttribute("id")) {
                 eleSecHome.style.display = "flex";
                 eleVideoBG.style.display = "inline-flex";
-                eleSorceVideoBG.setAttribute('src', '../Media/Video/Toaru-Kagaku-no-Accelerator.m4v');
+                eleSorceVideoBG.setAttribute('src', '../Media/Videos/Toaru-Kagaku-no-Accelerator.m4v');
                 eleVideoBG.load();
                 eleIHome.classList.add("active");
             } else { 
@@ -48,7 +48,7 @@ document.addEventListener('DOMContentLoaded', function(event) {
             if(nameSelection == eleSecAboutMe.getAttribute("id")) {
                 eleSecAboutMe.style.display = "flex";
                 eleVideoBG.style.display = "inline-flex";
-                eleSorceVideoBG.setAttribute('src', '../Media/Video/Toaru-Kagaku-no-Railgun.m4v');
+                eleSorceVideoBG.setAttribute('src', '../Media/Videos/Toaru-Kagaku-no-Railgun.m4v');
                 eleVideoBG.load();
                 eleIAboutMe.classList.add("active");
             } else {
@@ -60,7 +60,7 @@ document.addEventListener('DOMContentLoaded', function(event) {
             if(nameSelection == eleSecFeatures.getAttribute("id")) {
                 eleSecFeatures.style.display = "flex";
                 eleVideoBG.style.display = "inline-flex";
-                eleSorceVideoBG.setAttribute('src', '../Media/Video/Toaru-Majutsu-no-Index2.m4v');
+                eleSorceVideoBG.setAttribute('src', '../Media/Videos/Toaru-Majutsu-no-Index2.m4v');
                 eleVideoBG.load();
                 eleIFeatures.classList.add("active");
             } else { 
@@ -72,7 +72,7 @@ document.addEventListener('DOMContentLoaded', function(event) {
             if(nameSelection == eleSecDev.getAttribute("id")) {
                 eleSecDev.style.display = "flex";
                 eleVideoBG.style.display = "inline-flex";
-                eleSorceVideoBG.setAttribute('src', '../Media/Video/Toaru-Majutsu-no-Index1.m4v');
+                eleSorceVideoBG.setAttribute('src', '../Media/Videos/Toaru-Majutsu-no-Index1.m4v');
                 eleVideoBG.load();
                 eleIDev.classList.add("active");
             } else {
