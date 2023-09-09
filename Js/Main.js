1f3515b9afc6f3684147ab7812101eea9dcd5a47
Arutosio <30377429+Arutosio@users.noreply.github.com>
2023-09-09 15:20:23 +0200

    removed usles code

--- Js/Main.js
+++ Js/Main.js
@@ -288,16 +288,3 @@ function AppendEvents() {
 function ShowAddressInfo() {
     cryptoSelectOptions
 }
-
-function Fade(element) {
-    var op = 1;  // initial opacity
-    var timer = setInterval(function () {
-        if (op <= 0.1){
-            clearInterval(timer);
-            // element.style.display = 'none';
-        }
-        element.style.opacity = op;
-        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-        op -= op * 0.1;
-    }, 50);
-}
